c95435b49e5bd839a22da8cda9b05fc49414e627
dastangoo <hamed.dastangoo@gmail.com>
2017-08-17 19:38:53 +0430

    Importing questions from a JSON file

---
+++ json.js
@@ -0,0 +1,9 @@
+// JSON Document
+data = {"quizcontent": [
+	{"question": "question text 1", "image": "http://placehold.it/200x200", "correct": 1, "a1": "answer 1", "a2": "answer 2", "a3": "answer 3", "a4": "answer 4"},
+		{"question": "question text 2", "image": "http://placehold.it/200x200", "correct": 1, "a1": "answer 1", "a2": "answer 2", "a3": "answer 3", "a4": "answer 4"},
+		{"question": "question text 3", "image": "http://placehold.it/200x200", "correct": 1, "a1": "answer 1", "a2": "answer 2", "a3": "answer 3", "a4": "answer 4"},
+		{"question": "question text 4", "image": "http://placehold.it/300x300", "correct": 1, "a1": "answer 1", "a2": "answer 2", "a3": "answer 3", "a4": "answer 4"},
+		{"question": "question text 5", "image": "http://placehold.it/400x400", "correct": 1, "a1": "answer 1", "a2": "answer 2", "a3": "answer 3", "a4": "answer 4"}
+
+]}
